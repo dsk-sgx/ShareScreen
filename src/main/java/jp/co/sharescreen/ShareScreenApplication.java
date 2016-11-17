469eaf24c92073f71bed5baa7ee3fbd833f4532f
sugiyamads <daisuke1989ppp@gmail.com>
2016-11-17 23:18:54 +0900

    gradle、SpringBootアプリケーションに変更

---
+++ src/main/java/jp/co/sharescreen/ShareScreenApplication.java
@@ -0,0 +1,17 @@
+package jp.co.sharescreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+public class ShareScreenApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShareScreenApplication.class, args);
+  }
+}
